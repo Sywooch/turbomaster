96f86ab863794b1cf8d3a64d6e52e888306530c3
Oleg <oleg3012@yandex.ru>
2015-02-22 16:28:49 +0300

    fx-a

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 
+    alert($(window).width())
+    
+    // var hideTableColumns = function(target, columns, toggleWidth = 768) {
+       
+    //    $.each(columns, function(i, column) { 
+    //         var els =  $(target + ' td:nth-child(' + column + '), ' + target + ' th:nth-child(' + column + ')');
+    //         if($(window).width() < toggleWidth) {
+    //             els.hide();
+    //         }  else {
+    //             els.show();
+    //         }
+    //     });
+    // }
 
-    $('#table-popular').remove();
+    // hideTableColumns('#table-popular', [1, 3, 6]);
+
+    // $(window).resize(function(){
+    //      hideTableColumns('#table-popular', [1, 3, 6]);
+    // });
+
 
 
 
