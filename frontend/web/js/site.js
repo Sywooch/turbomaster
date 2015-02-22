2c2eb3c2f295e8c5d2ef524d316d06eca4a721c0
Oleg <oleg3012@yandex.ru>
2015-02-22 16:27:25 +0300

    fx-a

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -1,28 +1,7 @@
 $(document).ready(function() {
 
-    // $('.stylerize').styler();
-    alert('two')
-    var hideTableColumns = function(target, columns, toggleWidth = 768) {
-       
-       $(target).remove();
-       return;
 
-       $.each(columns, function(i, column) { 
-            var els =  $(target + ' td:nth-child(' + column + '), ' + target + ' th:nth-child(' + column + ')');
-            if($(window).width() < toggleWidth) {
-                els.hide();
-            }  else {
-                els.show();
-            }
-        });
-    }
-
-    hideTableColumns('#table-popular', [1, 3, 6]);
-
-    $(window).resize(function(){
-         hideTableColumns('#table-popular', [1, 3, 6]);
-    });
-
+    $('#table-popular').remove();
 
 
 
