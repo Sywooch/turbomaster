9617b3ed0fa9e11f7072ee0b34c52a84102c7f79
Oleg <oleg3012@yandex.ru>
2015-03-03 17:34:55 +0300

    dfhgfh

--- frontend/views/layouts/_footer.php
+++ frontend/views/layouts/_footer.php
@@ -1,58 +1,62 @@
 <footer>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="footer-bgr">
-                <div class="texture-overlay"></div>
-                <div class="container">
-                    <div class="row"  style="padding: 40px 20px 20px 20px;">
+    <div class="footer-wrap">
+        <div class="overlay-grid-dark">  
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <h4>Популярные турбины <br>для легковых автомобилей:</h4>
+                        <div class="tagcloud">
+                            <a href="/article/turbokit" title="Турбина для Нива">Нива</a>
+                            <a href="/turboshop/passenger/bmw" title="Турбина для БМВ">БМВ</a>
+                            <a href="/turboshop/passenger/audi" title="Турбина для Ауди">Ауди</a>
+                            <a href="/turboshop/passenger/chevrolet" title="Турбина для Шевроле">Шевроле</a>
+                            <a href="/turboshop/passenger/ford" title="Турбина для Форд">Форд</a>
+                            <a href="/turboshop/passenger/hyundai" title="Турбина для Хендай" >Хендай</a>
+                            <a href="/turboshop/passenger/kia" title="Турбина для КИА">КИА</a>
+                            <a href="/turboshop/passenger/land_rover" title="Турбина для Лэнд Ровер">Лэнд Ровер</a>
+                            <a href="/turboshop/passenger/mazda" title="Турбина для Мазда">Мазда</a>
+                            <a href="/turboshop/passenger/mercedes" title="Турбина для Мерседес">Мерседес</a>
+                            <a href="/turboshop/passenger/mitsubishi" title="Турбина для Мицубиши">Мицубиши</a>
+                            <a href="/turboshop/passenger/nissan" title="Турбина для Ниссан">Ниссан</a>
+                            <a href="/turboshop/passenger/opel" title="Турбина для Опель">Опель</a>
+                            <a href="/turboshop/passenger/peugeot" title="Турбина для Пежо">Пежо</a>
+                            <a href="/turboshop/passenger/renault" title="Турбина для Рено">Рено</a>
+                            <a href="/turboshop/passenger/citroen" title="Турбина для Ситроен">Ситроен</a>
+                            <a href="/turboshop/passenger/saab" title="Турбина для Сааб">Сааб</a>
+                            <a href="/turboshop/passenger/skoda" title="Турбина для Шкода">Шкода</a>
+                            <a href="/turboshop/passenger/subaru" title="Турбина для Субару">Субару</a>
+                            <a href="/turboshop/passenger/toyota" title="Турбина для Тойота">Тойота</a>
+                            <a href="/turboshop/passenger/volkswagen" title="Турбина для Фольксваген">Фольксваген</a>
+                            <a href="/turboshop/passenger/volvo" title="Турбина для Вольво">Вольво</a>
+                        </div>
+                    </div><!-- /.col-md-6 -->    
+                    <div class="col-md-4">
+                        <h4>Популярные турбины <br>для грузовых автомобилей:</h4>
+                        <div class="tagcloud">
+                            <a href="/turboshop/trucks/cummins" title="Турбина для Камминс">Камминс</a>
+                            <a href="/turboshop/trucks/daf" title="Турбина для ДАФ">ДАФ</a>
+                            <a href="/turboshop/trucks/ddc" title="Турбина для Детройт Дизель">Детройт Дизель</a>
+                            <a href="/turboshop/trucks/deutz" title="Турбина для Дойц ">Дойц</a>
+                            <a href="/turboshop/trucks/iveco" title="Турбина для Ивеко">Ивеко</a>
+                            <a href="/turboshop/trucks/komatsu" title="Турбина для Комацу">Комацу</a>
+                            <a href="/turboshop/trucks/man" title="Турбина для Ман">Ман</a>
+                            <a href="/turboshop/trucks/mercedes" title="Турбина для Мерседес">Мерседес</a>
+                            <a href="/turboshop/trucks/scania" title="Турбина для Сканиа">Сканиа</a>
+                        </div>
 
-            <section id="cloud">
-                
-                <h4 style="">Популярные турбины для легковых автомобилей</h4>
-                <div class="tagcloud">
-                    <a href="/article/turbokit" title="Турбина для Нива">Нива</a>
-                    <a href="/turboshop/passenger/bmw" title="Турбина для БМВ">БМВ</a>
-                    <a href="/turboshop/passenger/audi" title="Турбина для Ауди">Ауди</a>
-                    <a href="/turboshop/passenger/chevrolet" title="Турбина для Шевроле">Шевроле</a>
-                    <a href="/turboshop/passenger/ford" title="Турбина для Форд">Форд</a>
-                    <a href="/turboshop/passenger/hyundai" title="Турбина для Хендай" >Хендай</a>
-                    <a href="/turboshop/passenger/kia" title="Турбина для КИА">КИА</a>
-                    <a href="/turboshop/passenger/land_rover" title="Турбина для Лэнд Ровер">Лэнд Ровер</a>
-                    <a href="/turboshop/passenger/mazda" title="Турбина для Мазда">Мазда</a>
-                    <a href="/turboshop/passenger/mercedes" title="Турбина для Мерседес">Мерседес</a>
-                    <a href="/turboshop/passenger/mitsubishi" title="Турбина для Мицубиши">Мицубиши</a>
-                    <a href="/turboshop/passenger/nissan" title="Турбина для Ниссан">Ниссан</a>
-                    <a href="/turboshop/passenger/opel" title="Турбина для Опель">Опель</a>
-                    <a href="/turboshop/passenger/peugeot" title="Турбина для Пежо">Пежо</a>
-                    <a href="/turboshop/passenger/renault" title="Турбина для Рено">Рено</a>
-                    <a href="/turboshop/passenger/citroen" title="Турбина для Ситроен">Ситроен</a>
-                    <a href="/turboshop/passenger/saab" title="Турбина для Сааб">Сааб</a>
-                    <a href="/turboshop/passenger/skoda" title="Турбина для Шкода">Шкода</a>
-                    <a href="/turboshop/passenger/subaru" title="Турбина для Субару">Субару</a>
-                    <a href="/turboshop/passenger/toyota" title="Турбина для Тойота">Тойота</a>
-                    <a href="/turboshop/passenger/volkswagen" title="Турбина для Фольксваген">Фольксваген</a>
-                    <a href="/turboshop/passenger/volvo" title="Турбина для Вольво">Вольво</a>
-                </div>
-                
-                <h4 style="margin-top: 20px; margin-bottom: 4px;">Популярные турбины для грузовых автомобилей</h4>
-                <div class="tagcloud">
-                    <a href="/turboshop/trucks/cummins" title="Турбина для Камминс">Камминс</a>
-                    <a href="/turboshop/trucks/daf" title="Турбина для ДАФ">ДАФ</a>
-                    <a href="/turboshop/trucks/ddc" title="Турбина для Детройт Дизель">Детройт Дизель</a>
-                    <a href="/turboshop/trucks/deutz" title="Турбина для Дойц ">Дойц</a>
-                    <a href="/turboshop/trucks/iveco" title="Турбина для Ивеко">Ивеко</a>
-                    <a href="/turboshop/trucks/komatsu" title="Турбина для Комацу">Комацу</a>
-                    <a href="/turboshop/trucks/man" title="Турбина для Ман">Ман</a>
-                    <a href="/turboshop/trucks/mercedes" title="Турбина для Мерседес">Мерседес</a>
-                    <a href="/turboshop/trucks/scania" title="Турбина для Сканиа">Сканиа</a>
-                </div>
-            </section>
-
-            <section style="margin: 20px 0 10px 0;">
-                <div id="copyright">
-                    Компания "ТурбоМастер" © 2015
-                </div>
-            </section>
+                    </div><!-- /.col-md-6 -->
+                    <div class="col-md-3">
+                        <h4>О компании:</h4>
+                        <div id="copyright" style="margin-top: 14px; font-size: 13px;">
+                            "ТурбоМастер" © 2015<br>
+                            119034, г. Москва, Волгоградский проспект. д.32, к. 24, офис 208 <a href="/contact#map-turbomaster">(cхема проезда)</a><br>
+                            <span class="bold">+7 (499) 650-76-45</span>, 
+                            <span class="bold">+7 (963) 777-0949</span><br>
+                            Пн-Пт с 8:00 до 19:00<br> Сб с 10:00 до 14:00<br>
+                            e-mail: <a href="mailto:sales@turbomaster.ru">sales@turbomaster.ru</a><br>
+                        </div>
+                    </div>
+                </div><!-- /.row -->
 
             <section>
                 <!-- Yandex.Metrika counter -->
@@ -90,8 +94,6 @@
 
 
 
-                    </div><!-- /.row -->
-                </div><!-- /.row -->
             </div><!-- /.row -->
         </div><!-- /.row -->
     </div><!-- /.container -->
