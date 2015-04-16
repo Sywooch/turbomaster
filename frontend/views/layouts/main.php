aec120e6269710448be66799e750202b5f6ec266
Oleg <oleg3012@yandex.ru>
2015-04-16 18:20:37 +0300

    fx-a

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -30,10 +30,12 @@ AppAsset::register($this);
         <?= $this->render('_counts'); ?>
     <?php $this->endBody() ?>
     <!-- BEGIN JIVOSITE CODE {literal} -->
-    <script type='text/javascript'>
+    <!-- <script type='text/javascript'>
     (function(){ var widget_id = 'dT9qiIjnMV';
-    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script> -->
     <!-- {/literal} END JIVOSITE CODE -->
+
+    <script type="text/javascript">document.write('<script type="text/javascript" charset="utf-8" async="true" id="onicon_loader" src="http://cp.onicon.ru/js/simple_loader.js?site_id=552fd2d42866887e278b457b&srv=1&' + (new Date).getTime() + '"></scr' + 'ipt>');</script>
 </body>
 </html>
 <?php $this->endPage() ?>
