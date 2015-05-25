e1118551ace5b9214f534ef9be6f79febfbcca70
Oleg <oleg3012@yandex.ru>
2015-05-25 15:16:34 +0300

    add intro photo

--- frontend/views/site/_section_intro.php
+++ frontend/views/site/_section_intro.php
@@ -1,11 +1,13 @@
 <?php
 use yii\helpers\Html;
+
+$class_bg  = rand(1, 2);
 ?>
 
 <section id="main-intro">
     <div class="container-fluid">
         <div class="row">
-            <div class="intro-bg">
+            <div class="intro-bg bg-<?= $class_bg ?>">
                 <div class="texture-overlay"></div>
                     <div class="col-md-6">
                     </div>
