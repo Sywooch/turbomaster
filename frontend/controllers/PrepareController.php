d5bf619506ce3038634f7561e404225fdc2ea53f
Oleg <oleg3012@yandex.ru>
2015-07-11 10:00:33 +0300

    commit_stage_2015-07-11_10-00

--- frontend/controllers/PrepareController.php
+++ frontend/controllers/PrepareController.php
@@ -56,7 +56,7 @@ class PrepareController extends Controller
     }
 
 
-    ////////////// ffff dd ff  wwdd ////////////
+    ////////////// ffff dd ffff  wwdd ////////////
 
 
     public function actionSerializeRubric()
