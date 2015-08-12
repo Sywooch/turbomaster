9c6e3495e14a9fa2e80a53ee9cdd356096aa2e1c
Oleg <oleg3012@yandex.ru>
2015-08-12 12:07:29 +0300

    commit_stage_2015-08-12_12-07

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -14,7 +14,8 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-
+    <?= Html::cssFile('@web/css/style.min.css?v=' . filemtime(Yii::getAlias('@webroot/css/style.min.css'))) ?>
+    
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">   
